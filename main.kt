21aa423d7391c94c7f0c1fa00023c2ef04aed5f0
Omar Muhammad <aztenz@outlook.com>
2022-08-03 14:12:34 +0200

    [add] function
    adding function add(Int, Double) that adds an integer and a double

--- main.kt
+++ main.kt
@@ -1,3 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val num1 = 11
+    val num2 = 12.3
+    add(num1, num2)
+}
+fun add(num1: Int, num2: Double) {
+    val result = num1 + num2
+    println("Result is: $result")
 }
